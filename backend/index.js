85987bbd0b04423e242711c413663a3223a55eed
divyam751 <mi.divyam@gmail.com>
2024-05-10 11:43:26 +0530

    GenieAI backend

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home Route");
+});
+
+app.post("/genie", async (req, res) => {
+  let { prompt } = req.body;
+  // prompt += `, write markdown code file for your response`;
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  res.status(200).send({ text: text });
+});
+
+app.listen(8000, () => {
+  console.log("App started on port 8000");
+});
